06f3128c9d1ceb67e5344b1b9f2decbc679a26ae
KhrisDV <KhrisQ@protonmail.com>
2022-03-22 10:44:26 +0100

    buttons working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [ firstNumber, setFirstNumber ] = useState(null);
+  const [firstNumber, setFirstNumber] = useState(0);
 
-  function changeFirstNumberHandler (event) {
+  function changeFirstNumberHandler(event) {
     setFirstNumber(event.target.value);
   }
 
-  useEffect(
-    ()=>{
-      console.log("firstNumber state:", firstNumber);
-    }
-  )
+  const [secondNumber, setSecondNumber] = useState(0);
+
+  function changeSecondNumberHandler(event) {
+    setSecondNumber(event.target.value);
+  }
+
+  const [result, setResult] = useState(0);
+
+  function addHandler(event) {
+    setResult(parseFloat(firstNumber) + parseFloat(secondNumber));
+  }
+
+  function restHandler(event) {
+    setResult(parseFloat(firstNumber) - parseFloat(secondNumber));
+  }
+
+  function porHandler(event) {
+    setResult(parseFloat(firstNumber) * parseFloat(secondNumber));
+  }
+
+  function divHandler(event) {
+    setResult(parseFloat(firstNumber) / parseFloat(secondNumber));
+  }
+
+  function clearHandler(event) {
+    setResult(null);
+    setSecondNumber("");
+    setFirstNumber("");
+  }
+
+  useEffect(() => {
+    console.log("firstNumber state:", firstNumber);
+    console.log("secondNumber state:", secondNumber);
+    console.log("result state:", result);
+  });
 
   return (
     <>
       <h1>Calculadora</h1>
-      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <input
+        type="text"
+        value={firstNumber}
+        onChange={changeFirstNumberHandler}
+      />
+      <input
+        type="text"
+        value={secondNumber}
+        onChange={changeSecondNumberHandler}
+      />
+      <p>{result}</p>
+      <input type="button" value="+" onClick={addHandler} />
+      <input type="button" value="-" onClick={restHandler} />
+      <input type="button" value="x" onClick={porHandler} />
+      <input type="button" value="/" onClick={divHandler} />
+      <input type="button" value="C" onClick={clearHandler} />
     </>
   );
 }
